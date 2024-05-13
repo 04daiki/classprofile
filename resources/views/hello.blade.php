0101528a86508bb5d747a94677ed1f05f974132c
04daiki <nuuutaro@gmail.com>
2024-05-13 10:06:43 +0900

    helloworldの表示

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+</head>
+
+<body>
+    <h1>HelloWorldでました</h1>
+</body>
